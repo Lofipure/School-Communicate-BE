86e383d3a8fe84fd7806c44007ee1e39ca5b6fba
wangziheng.0915 <wangziheng.0915@bytedance.com>
2021-07-06 09:15:29 +0800

    feat(*): ckrea

--- dbConnection.js
+++
@@ -1,8 +0,0 @@
-const {Sequelize} = require("sequelize");
-
-const connection = new Sequelize('student_communicate_system', 'root', '12345678', {
-    host: 'localhost',
-    dialect: 'mysql'
-});
-
-module.exports = connection;
